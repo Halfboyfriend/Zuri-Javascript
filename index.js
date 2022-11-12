580259da8bf5d40d1842d47e767a335ffa13e4c9
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-11-12 17:49:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+let slidePosition = 0;
+const slide = document.getElementsByClassName('carousel_item');
+const totalSlide = slide.length;
 
+//console.log(totalSlide);
+document.getElementById('carousel_button--next')
+.addEventListener("click", function(){
+    moveToNextSlide();
+});
+document.getElementById('carousel_button--prev')
+.addEventListener("click", function(){
+    moveToPrevSlide();
+});
+
+function updateSlide(){
+    for (let slides of slide)
+    {
+        slides.classList.remove('carousel_item--visible');
+        slides.classList.add('carousel_item--hidden');
+    }
+    slide[slidePosition].classList.add('carousel_item--visible');
+}
+
+
+function  moveToNextSlide(){
+    if (slidePosition == totalSlide - 1)
+    {
+        slidePosition = 0;
+    } else{
+        slidePosition++;
+    }
+
+    updateSlide();
+}
+
+function moveToPrevSlide(){
+    if (slidePosition == 0)
+    {
+        slidePosition = totalSlide - 1;
+    } else{
+        slidePosition--;
+    }
+
+    updateSlide();
+}
